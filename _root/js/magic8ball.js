152f9d4e6566aaf4d61ab84cd1fe83f82dc3c79b
Kris Burton <burton.kris@outlook.com>
2018-05-11 11:42:15 +0100

    magic8ball.js

---
+++ _root/js/magic8ball.js
@@ -0,0 +1,4 @@
+const answer = ['Ask again later', 'Yes', 'No', 'Maybe', 'Keep wishing', 'Doesn\'t look good', ] 
+let result = answer[Math.floor(Math.random()*answer.length)];
+  
+console.log(result);
